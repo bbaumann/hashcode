0032e709bf3613aeb34b004b392186e8c79b8e7f
Fabrice Peltier <f.peltier@criteo.com>
2018-03-01 21:45:39 +0100

    stuff

---
+++ hashcode.march/RideSlicer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using hashcode.march.Models;
+
+namespace hashcode.march
+{
+    class RideSlicer
+    {
+        List<Ride> rides;
+   
+        public RideSlicer(State state)
+        {
+            RideAbsComp rideAbsComp = new RideAbsComp();
+            List<Ride> rides = state.rides;
+            rides.Sort(rideAbsComp);
+        }
+
+        public List<Ride> Slice(int inMaxItem, int inMaxStep)
+        {
+            int i;
+            for (i = 0; i < rides.Count && i < inMaxItem; ++i)
+            {
+                if (inMaxStep < rides[i].EarliestStart)
+                {
+                    break;
+                }
+            }
+            return rides.GetRange(0, i);
+        }
+    }
+}
