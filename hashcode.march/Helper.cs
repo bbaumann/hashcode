b49ffef2a6938b7fba5789e8a73569446cd69cb3
Benjamin Baumann <b.baumann@criteo.com>
2018-03-01 21:10:29 +0100

    files

--- hashcode.march/Helper.cs
+++ hashcode.march/Helper.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace hashcode.march
 {
     class Helper
-    {        public static void ConsoleLog(string inMsg)
+    {
+        public static void ConsoleLog(string inMsg)
         {
-            Console.WriteLine(inMsg);
+            //Console.WriteLine(inMsg);
+            File.AppendAllText(@"result.txt", inMsg + Environment.NewLine);
         }
     }
 }
