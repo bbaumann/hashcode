d529240eaf5ae3be5843353c1ffcdbbefd9696b8
Benjamin Baumann <b.baumann@criteo.com>
2020-02-20 19:10:04 +0100

    Order books

--- hashcode.2020/Models/Book.cs
+++ hashcode.2020/Models/Book.cs
@@ -1,9 +1,16 @@
-﻿namespace hashcode._2020.Models
+﻿using System;
+
+namespace hashcode._2020.Models
 {
-    public class Book
+    public class Book : IComparable<Book>
     {
         public int Id { get; set; }
 
         public int Score { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            return Score.CompareTo(other.Score);
+        }
     }
 }
