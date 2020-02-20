85dcb9466c3cf1c6eb170640bb061946486a6818
Fabrice Peltier <f.peltier@criteo.com>
2020-02-20 19:09:18 +0100

    parsing

--- hashcode.2020/Models/StateFactory.cs
+++ hashcode.2020/Models/StateFactory.cs
@@ -18,11 +18,48 @@ namespace hashcode._2020.Models
             string[] lines = s.Split('\n');
             string[] inputs = lines[0].Split(' ');
             //Parse the first line here here
-            
-            for (int lineIndex = 1; lineIndex < lines.Length; lineIndex++)
+
+            var bookCount = int.Parse(inputs[0]);
+            var libCount = int.Parse(inputs[1]);
+            state.NbDays = int.Parse(inputs[2]);
+
+            // books score
+            int lineIndex = 1;
+            inputs = lines[lineIndex].Split(' ');
+
+            for (var i_in = 0; i_in < bookCount; ++i_in)
+            {
+                state.ScoreByBookId.Add(i_in, int.Parse(inputs[i_in]));
+            }
+            ++lineIndex;
+
+            var libIndex = 0;
+            var isLibDef = true;
+            for (; lineIndex < lines.Length && libIndex < libCount; lineIndex++)
             {
                 inputs = lines[lineIndex].Split(' ');
-                //Parse the line here
+
+                if (isLibDef)
+                {
+                    state.Libraries[libIndex] = new Library
+                    {
+                        Freq = int.Parse(inputs[2]),
+                        NbDaysToSignup = int.Parse(inputs[1])
+                    };
+                } else {
+                    state.Libraries[libIndex].Books = new List<Book>();
+                    for (var i_in = 0; i_in < inputs.Length; ++i_in)
+                    {
+                        var id = int.Parse(inputs[i_in]);
+                        state.Libraries[libIndex].Books.Add(new Book {
+                            Id = id,
+                            Score = state.ScoreByBookId[id]
+                        });
+                    }
+                    ++libIndex;
+                }
+
+                isLibDef = !isLibDef;
             }
 
             return state;
