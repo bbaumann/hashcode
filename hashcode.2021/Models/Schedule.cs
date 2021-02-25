234ec09689ce63093ae62bec1a63f390bf378601
Gregory Riberon <g.riberon@criteo.com>
2021-02-25 19:17:04 +0100

    Add Intersection in schedule

--- hashcode.2021/Models/Schedule.cs
+++ hashcode.2021/Models/Schedule.cs
@@ -6,6 +6,7 @@ namespace hashcode._2021.Models
 {
     public class Schedule
     {
+        public Intersection Intersection { get; set; }
         public List<Tuple<string,int>> GreenDurationByStreetName { get; set; }
     }
 }
