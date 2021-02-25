bdab33d8bf36c053e7563e0929da75f5abb7afd1
Benjamin Baumann <b.baumann@criteo.com>
2021-02-25 20:27:09 +0100

    Get green street at a time

--- hashcode.2021/Models/Schedule.cs
+++ hashcode.2021/Models/Schedule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,22 @@ namespace hashcode._2021.Models
     {
         public Intersection Intersection { get; set; }
         public List<Tuple<string,int>> GreenDurationByStreetName { get; set; }
+
+        private int cycleDuration => GreenDurationByStreetName.Sum(t => t.Item2);
+
+        public string GreenStreet(int time)
+        {
+            var pointInCycle = time % cycleDuration;
+            int accu = 0;
+            foreach (var cycleStep in GreenDurationByStreetName)
+            {
+                accu += cycleStep.Item2;
+                if (pointInCycle < accu)
+                {
+                    return cycleStep.Item1;
+                }
+            }
+            throw new Exception("Error in Schedule");
+        }
     }
 }
