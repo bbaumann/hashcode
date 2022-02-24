7afafaa272a15f8da63bd0e292a8ba4b2decf746
Benjamin Baumann <b.baumann@criteo.com>
2022-02-24 18:57:10 +0100

    Contributor

---
+++ hashcode.2022/Models/Contributor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashcode._2022.Models
+{
+    class Contributor
+    {
+        public string Name { get; set; }
+
+        public Dictionary<string,int> Skills { get; init; }
+
+        public void AddSkill(string skill, int level)
+        {
+            Skills[skill] = level;
+        }
+
+        public void IncreaseSkill(string skill)
+        {
+            if (Skills.ContainsKey(skill))
+                Skills[skill]++;
+            else
+                Skills[skill] = 1;
+        }
+    }
+}
