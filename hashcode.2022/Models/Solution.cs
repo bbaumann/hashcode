0717cea9247fe16af0b852ed1f48e35487c130b9
mrptipois <pti.pois@hotmail.fr>
2022-02-24 19:17:00 +0100

    Implement score computation.

--- hashcode.2022/Models/Solution.cs
+++ hashcode.2022/Models/Solution.cs
@@ -8,6 +8,15 @@ namespace hashcode._2022.Models
 {
     public class Solution : ISolution<State>
     {
+        class ProjectDone
+        {
+            public Project _proj;
+            public int _startDate = -1;
+            public List<Contributor> _contributor = new List<Contributor> ();
+        }
+
+        List<ProjectDone> _projectDone = new List<ProjectDone> ();
+
         State state = null;
         public Solution(State state)
         {
@@ -31,8 +40,15 @@ namespace hashcode._2022.Models
         /// <returns></returns>
         public double Value(State s)
         {
-            //TODO
-            return 0d;
+            int score = 0;
+            foreach(var projDone in _projectDone)
+            {
+                int endDate = projDone._startDate + projDone._proj.Duration;
+                int malus = Math.Max(endDate - projDone._proj.BestBefore, 0);
+                score += Math.Max(projDone._proj.Score - malus, 0);
+            }
+
+            return score;
         }
     }
 }
